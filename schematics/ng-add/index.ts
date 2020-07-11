4434c6f2eca4fc3e51f7c483c77410a5da2a312f
Khoi Bui <emquacau123472>
2020-07-11 15:29:49 -0400

    fix: 🐛 Fix ng-add schematics

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -1,107 +1,52 @@
-import { chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import {
-  addPackageJsonDependency,
-  getAppModulePath,
-  getProjectFromWorkspace,
-  getWorkspace,
-  InsertChange,
-  NodeDependency,
-  NodeDependencyType,
-  addModuleImportToRootModule,
-} from 'schematics-utilities';
-import * as ts from 'typescript';
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 
-import { insertImport } from './utils';
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { Schema } from './schema';
+
+import {
+  addModuleImportToRootModule,
+  addPackageToPackageJson,
+  getProjectFromWorkspace,
+  installPackageJsonDependencies,
+} from '../utils';
+
+const libName = '@iresa/ngx-imagely';
+const libVersion = '2.0.2';
+const moduleName = 'NgxImagelyModule';
+const modulePath = libName;
+
+/* tslint:disable-next-line: no-default-export */
+export default function (options: Schema): Rule {
+  return chain([addPackageJsonDependencies(), installPackageJsonDependencies(), addModuleToImports(options.project)]);
+}
 
 function addPackageJsonDependencies(): Rule {
   return (host: Tree, context: SchematicContext) => {
-    const dependencies: NodeDependency[] = [
-      {
-        type: NodeDependencyType.Default,
-        name: '@iresa/ngx-imagely',
-        version: '2.0.0',
-      },
-    ];
+    const dependencies: { name: string; version: string }[] = [{ name: libName, version: libVersion }];
 
     dependencies.forEach((dependency) => {
-      addPackageJsonDependency(host, dependency);
-      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+      addPackageToPackageJson(host, dependency.name, `${dependency.version}`);
+      context.logger.log('info', `✅️ Added "${dependency.name}`);
     });
 
     return host;
   };
 }
 
-function installPackageJsonDependencies(): Rule {
-  return (host: Tree, context: SchematicContext) => {
-    context.addTask(new NodePackageInstallTask());
-    context.logger.log('info', `🔍 Installing package...`);
-
-    return host;
-  };
-}
-
-function getTsSourceFile(host: Tree, path: string): ts.SourceFile {
-  const buffer = host.read(path);
-  if (!buffer) {
-    throw new SchematicsException(`Could not read file (${path}).`);
-  }
-  const content = buffer.toString();
-  const source = ts.createSourceFile(path, content, ts.ScriptTarget.Latest, true);
-
-  return source;
-}
-
-function injectImports(): Rule {
+function addModuleToImports(projectName: string | undefined): Rule {
   return (host: Tree) => {
     const workspace = getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace, Object.keys(workspace.projects)[0]);
-    const modulePath = getAppModulePath(host, (project as any).architect.build.options.main);
+    const project = getProjectFromWorkspace(workspace, projectName);
 
-    const moduleSource = getTsSourceFile(host, modulePath);
-
-    const change = insertImport(moduleSource, modulePath, 'NgxImagelyModule', '@iresa/ngx-imagely');
-
-    if (change) {
-      const recorder = host.beginUpdate(modulePath);
-      recorder.insertLeft((change as InsertChange).pos, (change as InsertChange).toAdd);
-      host.commitUpdate(recorder);
-    }
+    addModuleImportToRootModule(host, moduleName, modulePath, project);
 
     return host;
   };
-}
-
-function addModuleToImports(): Rule {
-  return (host: Tree, context: SchematicContext) => {
-    const workspace = getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace, Object.keys(workspace.projects)[0]);
-
-    const moduleImport = `NgxImagelyModule`;
-
-    addModuleImportToRootModule(host, moduleImport, null as any, project);
-
-    context.logger.log('info', `🌈 @iresa/ngx-imagely is imported`);
-
-    return host;
-  };
-}
-
-function log(): Rule {
-  return (host: Tree, context: SchematicContext) => {
-    context.logger.log('info', `👏 @iresa/ngx-imagely ready to use`);
-
-    return host;
-  };
-}
-
-export default function imagelyNgAdd(): Rule {
-  return chain([
-    addPackageJsonDependencies(),
-    installPackageJsonDependencies(),
-    addModuleToImports(),
-    injectImports(),
-    log(),
-  ]);
 }
--- schematics/ng-add/schema.ts
+++ schematics/ng-add/schema.ts
@@ -1,1 +1,18 @@
-export interface Schema {}
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export interface Schema {
+  project?: string;
+  component?: string;
+  ModuleOptions?: string;
+  module?: string;
+  name: string;
+  flat?: boolean;
+  path?: string;
+  skipImport?: boolean;
+}
